26222b87de114246972928e8b5666c14aa2717c4
AAChemezov <a.a.chemezov@gmail.com>
2019-02-14 07:56:20 +0800

    1.3. JavaDoc. Code convention[#112811]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
- package ru.job4j.calculate;
- 
+package ru.job4j.calculate;
+/**
+* Пример "Hello world'a" на языке Java.
+* Class Calculate решение задачи  урока 1.3. части 001 учебного курса на job4j.ru
+* @author Andrey Chemezov
+* @since 14.02.2019
+*/
 public class Calculate {
+/**
+* Method Main Точка входа в приложение. Выводит в консоль "Hellow World"
+* @param args Массив строковых параметров. 
+*/
 	public static void main (String[] args){
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Chemezov (a.a.chemezov@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
