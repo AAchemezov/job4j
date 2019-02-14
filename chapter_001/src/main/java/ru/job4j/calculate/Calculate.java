a7bbca18b24e2ef91f3d5421c052d3afb51877f7
AAChemezov <a.a.chemezov@gmail.com>
2019-02-14 08:52:35 +0800

    1.4. JUnit[#112812]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,12 +6,21 @@ package ru.job4j.calculate;
 * @since 14.02.2019
 */
 public class Calculate {
-/**
-* Method Main Точка входа в приложение. Выводит в консоль "Hellow World"
-* @param args Массив строковых параметров. 
-*/
+	
+	/**
+	* Method Main Точка входа в приложение. Выводит в консоль "Hellow World"
+	* @param args Массив строковых параметров. 
+	*/
 	public static void main (String[] args){
 		System.out.println("Hello World");
 	}
 	
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
 }
