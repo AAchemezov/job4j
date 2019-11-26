cb2a22803a3132744d059b05fed837b996c87a1c
AAchemesov <a.a.chemezov@gmail.com>
2019-11-26 13:12:13 +0800

    5.1.1. Итератор для двухмерного массива int[][][#124904]

---
+++ chapter_001/src/main/java/iterator/MatrixIterator.java
@@ -0,0 +1,45 @@
+package iterator;
+
+import java.util.Iterator;
+
+/**
+ * Реализация итератора для двухмерного массива.
+ *
+ * @author Andrey Chemezov
+ * @since 26.11.2019
+ */
+public class MatrixIterator implements Iterator {
+    private int[][] matrix;
+    private int next;
+
+    public MatrixIterator(int[][] matrix) {
+        super();
+        this.matrix = matrix;
+    }
+
+    @Override
+    public boolean hasNext() {
+        int sum = 0;
+        for (int[] arr : matrix) {
+            sum += arr.length;
+            if (next < sum) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        int sum = next;
+        for (int[] arr : matrix) {
+            if (sum >= arr.length) {
+                sum -= arr.length;
+                continue;
+            }
+            next++;
+            return arr[sum];
+        }
+        return null;
+    }
+}
