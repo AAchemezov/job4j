696cc038d7676fdc0430fa2cb1a541ee18a4ec64
AAchemesov <a.a.chemezov@gmail.com>
2020-01-06 19:14:51 +0800

    2. Не перекрывать equals hashCode[#124917]

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,33 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     String name;
     int children;
     Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Иван", 0, new GregorianCalendar(1999, Calendar.JANUARY, 25));
+        User user2 = new User(user1.name, user1.children, user1.birthday);
+
+        Map<User, Object> hashMap = new HashMap<>() {
+            {
+                put(user1, "User1");
+                put(user2, "User2");
+            }
+        };
+        System.out.println("user1.hashCode(): " + user1.hashCode());
+        System.out.println("user2.hashCode(): " + user2.hashCode());
+        System.out.println(hashMap);
+    }
 }
