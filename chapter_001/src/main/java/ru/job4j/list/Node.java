1d88fa322e98527a59ef347a142ed7f56e891af6
AAchemesov <a.a.chemezov@gmail.com>
2020-01-06 12:42:29 +0800

    5.3.4. Задан связанный список. Определить цикличность.[#124898]
    *Правки по ревью

--- chapter_001/src/main/java/ru/job4j/list/Node.java
+++ chapter_001/src/main/java/ru/job4j/list/Node.java
@@ -1,8 +1,8 @@
 package ru.job4j.list;
 
 public class Node<T> {
-    private static int cycleFindIndex;
-    private int cycleIndex;
+//    private static int cycleFindIndex;
+//    private int cycleIndex;
 
     T value;
     Node<T> next;
@@ -11,17 +11,37 @@ public class Node<T> {
         this.value = value;
     }
 
+//    /**
+//     * Реализация алгоритма, определяющего что список содержит замыкания
+//     */
+//    static boolean hasCycleMy(Node first) {
+//        cycleFindIndex++;
+//        while (first != null) {
+//            if (first.cycleIndex == cycleFindIndex) {
+//                return true;
+//            } else {
+//                first.cycleIndex = cycleFindIndex;
+//                first = first.next;
+//            }
+//        }
+//        return false;
+//    }
+
     /**
      * Реализация алгоритма, определяющего что список содержит замыкания
      */
     static boolean hasCycle(Node first) {
-        cycleFindIndex++;
-        while (first != null) {
-            if (first.cycleIndex == cycleFindIndex) {
+        Node rabbit = first;
+        Node turtle = first;
+        boolean upSpeed = false;
+        while (rabbit != null) {
+            rabbit = rabbit.next;
+            if (upSpeed) {
+                turtle = turtle.next;
+            }
+            upSpeed = !upSpeed;
+            if (rabbit == turtle) {
                 return true;
-            } else {
-                first.cycleIndex = cycleFindIndex;
-                first = first.next;
             }
         }
         return false;
